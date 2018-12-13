efc5826ff63fdcc1f95fc9905882b5c6aff7fe1d
Alperen ARICI <alparicieren@gmail.com>
2018-12-13 22:47:08 +0300

    Add project files.

---
+++ NoteLibrary/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NoteLibrary.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
