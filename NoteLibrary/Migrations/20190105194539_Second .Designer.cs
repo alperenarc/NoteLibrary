a4bb2eac324165f3df545695b94d69caf408f132
Alperen ARICI <alparicieren@gmail.com>
2019-01-05 22:47:52 +0300

    controller Added

--- NoteLibrary/Migrations/20190105194539_Second .Designer.cs
+++ NoteLibrary/Migrations/20190105194539_Second .Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NoteLibrary.Models.Contexts;
 
 namespace NoteLibrary.Migrations
 {
     [DbContext(typeof(NoteContext))]
-    partial class NoteContextModelSnapshot : ModelSnapshot
+    [Migration("20190105194539_Second ")]
+    partial class Second
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
