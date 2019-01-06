b48db47ebd13630c66d3f6520b67d18885e746ec
Alperen ARICI <alparicieren@gmail.com>
2019-01-06 13:49:08 +0300

    Registe page Added Half

---
+++ NoteLibrary/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NoteLibrary.Models.Contexts;
+using NoteLibrary.Models.Entities;
+
+namespace NoteLibrary.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly NoteContext _context;
+
+        public AccountController(NoteContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Account
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        // GET: Account/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Account/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register([Bind("Name,Surname,City,University,Department,Email,Password,Id,State")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+    }
+}
--- NoteLibrary/Models/Entities/User.cs
+++ NoteLibrary/Models/Entities/User.cs
@@ -9,22 +9,26 @@ namespace NoteLibrary.Models.Entities
     public class User:Entity
     {
         [Required]
+        [Display(Name = "Ad")]
         public string Name { get; set; }
         [Required]
+        [Display(Name = "Soyad")]
         public string Surname { get; set; }
-        //[Key]
-        //[Required]
-        //public string UserName { get; set; }
         [Required]
+        [Display(Name = "Şehir")]
         public string City { get; set; }
         [Required]
+        [Display(Name = "Üniversite")]
         public string University { get; set; }
         [Required]
+        [Display(Name = "Bölüm")]
         public string Department { get; set; }
         [Required]
+        [Display(Name = "Email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
         [Required]
+        [Display(Name = "Şifre")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
