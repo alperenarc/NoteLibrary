d09739dc5f83925a6bca148897b4b4305b0aa9a5
Alperen ARICI <alparicieren@gmail.com>
2019-01-10 13:38:38 +0300

    Home Index added

---
+++ NoteLibrary/Controllers/FileController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NoteLibrary.Models.Contexts;
+using NoteLibrary.Models.Entities;
+
+namespace NoteLibrary.Controllers
+{
+    public class FileController : Controller
+    {
+        private readonly NoteContext _context;
+
+        public FileController(NoteContext context)
+        {
+            _context = context;
+        }
+
+        // GET: File
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.FileTable.ToListAsync());
+        }
+
+        // GET: File/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var file = await _context.FileTable
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (file == null)
+            {
+                return NotFound();
+            }
+
+            return View(file);
+        }
+
+        // GET: File/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: File/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CourseName,Title,Description,FilePath,UploadDate,Id,State")] File file)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(file);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(file);
+        }
+
+        // GET: File/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var file = await _context.FileTable.FindAsync(id);
+            if (file == null)
+            {
+                return NotFound();
+            }
+            return View(file);
+        }
+
+        // POST: File/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CourseName,Title,Description,FilePath,UploadDate,Id,State")] File file)
+        {
+            if (id != file.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(file);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FileExists(file.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(file);
+        }
+
+        // GET: File/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var file = await _context.FileTable
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (file == null)
+            {
+                return NotFound();
+            }
+
+            return View(file);
+        }
+
+        // POST: File/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var file = await _context.FileTable.FindAsync(id);
+            _context.FileTable.Remove(file);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FileExists(int id)
+        {
+            return _context.FileTable.Any(e => e.Id == id);
+        }
+    }
+}
