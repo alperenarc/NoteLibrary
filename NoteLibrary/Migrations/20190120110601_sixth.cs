6e3cb9f3c1ed582b2ae32fc238cc19d6b4875bfb
Alperen ARICI <alparicieren@gmail.com>
2019-01-20 17:59:50 +0300

    Password Hashed Added

---
+++ NoteLibrary/Migrations/20190120110601_sixth.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NoteLibrary.Migrations
+{
+    public partial class sixth : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfirmPassword",
+                table: "UserTable");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "UserTable");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Hash",
+                table: "UserTable",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hash",
+                table: "UserTable");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConfirmPassword",
+                table: "UserTable",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "UserTable",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
